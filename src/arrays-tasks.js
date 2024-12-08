f6b5c6f454d1a3c914606e9a3348cdfd8190eca2
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-12-08 21:08:33 +0300

    add some tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -8,8 +8,7 @@
  ******************************************************************************************** */
 
 /**
- * Creates an array of integers from the specified start to end (inclusive).
- *
+ *Creates an array of integers from the specified start to end (inclusive).
  * @param {number} start - The first number of an array.
  * @param {number} end - The last number of an array.
  * @return {array} - An array of integers.
@@ -20,8 +19,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const newMass = [];
+  Array.from({ length: end - start + 1 }).map((_, i) =>
+    newMass.push(start + i)
+  );
+  return newMass;
 }
 
 /**
@@ -37,8 +40,13 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length > arr2.length) {
+    return arr1.map((el, i) => el + (arr2[i] || 0));
+  }
+  return arr2.map((el, i) => {
+    return el + (arr1[i] || 0);
+  });
 }
 
 /**
@@ -53,8 +61,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value) || -1;
 }
 
 /**
@@ -71,8 +79,15 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  let count = 0;
+  arr.map((el) => {
+    if (el === item) {
+      count += 1;
+    }
+    return null;
+  });
+  return count;
 }
 
 /**
@@ -87,8 +102,13 @@ function findAllOccurrences(/* arr, item */) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((el) => {
+    if (el) {
+      return el;
+    }
+    return null;
+  });
 }
 
 /**
@@ -101,8 +121,13 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((el) => {
+    if (el) {
+      return el.length;
+    }
+    return 0;
+  });
 }
 
 /**
@@ -119,8 +144,14 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const sum = arr.reduce((acc, el) => {
+    return acc + el;
+  }, 0);
+  return parseFloat((sum / arr.length).toFixed(2));
 }
 
 /**
