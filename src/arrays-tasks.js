b12962105f16f0ded744cabcd7ce7b211a2489dc
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-12-10 19:11:49 +0300

    add some tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -534,8 +534,10 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((el) => {
+    return `#${el.toString(16).padStart(6, '0').toUpperCase()}`;
+  });
 }
 
 /**
@@ -552,8 +554,8 @@ function getHexRGBValues(/* arr */) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  return arr.sort((a, b) => b - a).slice(0, n);
 }
 
 /**
@@ -568,8 +570,10 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.filter((el) => {
+    return arr2.includes(el);
+  });
 }
 
 /**
@@ -583,8 +587,16 @@ function findCommonElements(/* arr1, arr2 */) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  const subseqCounts = nums.reduce((acc, currEl, i) => {
+    if (i === 0 || currEl <= nums[i - 1]) {
+      acc.push(1);
+    } else {
+      acc[acc.length - 1] += 1;
+    }
+    return acc;
+  }, []);
+  return Math.max(...subseqCounts);
 }
 
 /**
