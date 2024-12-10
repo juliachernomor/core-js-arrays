c7e7a32c294b8687ce1e8f88457a5970a42b5e45
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-12-10 17:51:33 +0300

    add some tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -485,8 +485,19 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from(
+    {
+      length: n,
+    },
+    (_, i) =>
+      Array.from(
+        {
+          length: n,
+        },
+        (val, j) => (i === j ? 1 : 0)
+      )
+  );
 }
 
 /**
@@ -500,8 +511,17 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .filter((el) => {
+      if (el % 2 !== 0) {
+        return el;
+      }
+      return null;
+    })
+    .map((_, i) => {
+      return 2 * i || 0;
+    });
 }
 
 /**
